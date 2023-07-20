5194094e0c70542013dbdab8565d9efab428be72
Stephan Kolontay <stepan.kolontaj@gmail.com>
2023-07-20 17:49:38 +0300

    ino file update

--- App.ino
+++ App.ino
@@ -2,8 +2,7 @@
 #include "display.h"
 #include "LogTask.h"
 
-
-LogTask	logger;		
+LogTask	logger;
 Control control;
 KeypadTask keypad;
 Display lc_display;
@@ -14,16 +13,16 @@ void UserInit()
   Serial.begin(115200);
   
   lc_display.Start();
-  
+
   RPS::Init();
-  
+
   PWM::Init();
-  
+
   logger.Start();
-  
+
   control.Start();
 
-  if (logger.SetDate(3,12,2,10,10,10))
+  if (logger.SetDate(3, 12, 2, 10, 10, 10))
     return;
 
   Kernel::OS.MessageQueue.Post(MSG_ID_INIT_COMPLETE, 0, Kernel::MQ_OWNER_CALLER, Kernel::MQ_CONTEXT_TASK);
